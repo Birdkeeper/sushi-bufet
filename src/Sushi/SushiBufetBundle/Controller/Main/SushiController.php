f698585f1afa0af5362b36358f990e34b68b75c7
Ekaterina Kolidenkova <kolidenkova@dvhab.ru>
2018-10-03 16:32:22 +1000

    BK-0003: новый бандл, работоспособный

---
+++ src/Sushi/SushiBufetBundle/Controller/Main/SushiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sushi\SushiBufetBundle\Controller\Main;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @Route("/", service="sishi_bufet.controller.sushi")
+ */
+class SushiController extends Controller
+{
+
+}
